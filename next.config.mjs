77c82fb28b1bd9125b128d89af1c03e0db6b660e
Ashsajal <ashsajal1@gmail.com>
2026-03-06 17:38:56 +0600

    refactor: remove unused feats

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
@@ -14,11 +12,3 @@ const nextConfig = {
     ],
   },
 };
-
-export default withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  silent: true,
-  tunnelRoute: "/monitoring",
-  widenClientFileUpload: true,
-});
